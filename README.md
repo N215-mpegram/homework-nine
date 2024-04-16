49ce6919560ccf8bb0e15d74c5d6b8ca1585d734
maxpeg8 <maxpeg8@gmail.com>
2024-04-16 12:10:47 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Homework 9 Repo
+
+This is my homework-nine repo for N215.
+
+## Underneath is my hw9 link
+
+https://in-info-web4.informatics.iupui.edu/~mpegram/N215/homework-nine/index.html
---
+++ app/app.js
@@ -0,0 +1,240 @@
+var homeContent = `      <div class="home">
+<div class="hero">
+  <p class="heroTitle">EXPLORE</p>
+  <p class="heroTitle2">THE WORLD</p>
+</div>
+</div>`
+
+var aboutContent = `      <div class="about">
+<h1>ABOUT PAGE</h1>
+<div class="whatDo">
+  <h2>What Do We Do?</h2>
+  <p class="whatText">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
+    dolorum vero voluptates nostrum voluptatibus aut quo soluta vel
+    repellat possimus minima magni harum eveniet quaerat mollitia
+    doloremque perferendis, aspernatur laudantium. Amet eaque itaque
+    odio non, nihil eum nam iusto! Sapiente, consectetur libero.
+    Cupiditate nulla, at, distinctio magni dolorum officiis dicta facere
+    ad ipsum debitis in. Cumque culpa itaque atque quas maxime, corporis
+    iusto minima voluptatum quis amet officiis quibusdam sed rem
+    pariatur!
+  </p>
+  <p class="whatText">
+    Atque modi sit ipsam reiciendis commodi vel ab obcaecati cumque
+    minus ex quos id autem eaque excepturi, quia recusandae deserunt
+    dolores omnis sunt nobis? Sapiente officia quas soluta aspernatur
+    provident dignissimos eum sequi laborum. Quam ab magni voluptatum
+    nisi fugiat debitis id nesciunt ratione reprehenderit quaerat ipsum
+    sapiente voluptate, dolorem quidem quibusdam nobis, amet sunt
+    reiciendis nostrum adipisci maxime excepturi. Magnam, maxime
+    voluptatibus enim quibusdam in, dignissimos aperiam perferendis
+    suscipit est commodi animi dolores non nemo delectus illo aliquam
+    similique cumque, obcaecati pariatur et odio. Suscipit voluptates,
+    perspiciatis dicta iste mollitia, saepe accusantium ratione nihil
+    magnam in porro deleniti neque.
+  </p>
+  <p class="whatText">
+    Dolore voluptatum iste natus dolor veritatis facere sapiente, et
+    omnis dolorum! Labore consequuntur fugiat iure expedita delectus
+    rem, explicabo sequi maiores quae quidem laborum consequatur dolores
+    voluptate soluta ducimus qui sit eaque iusto quaerat repudiandae
+    nisi vel! Molestiae blanditiis quis commodi repellendus ipsam
+    distinctio quo suscipit placeat, ab, natus dolor consequuntur
+    consectetur veritatis ex doloribus possimus sequi non! Voluptate
+    dolorum reiciendis molestias libero id omnis, expedita eveniet
+    animi, nam magnam eligendi est reprehenderit! Quia?
+  </p>
+</div>
+<h2 class="whoText">Who Are We?</h2>
+<div class="place">
+  <img src="images/lebron.jpg" alt="Lebron" />
+  <div class="placeText">
+    <p class="aboutName">Lebron James</p>
+    <p class="aboutInfo">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos
+      quidem ratione doloribus tempora explicabo laudantium atque odit
+      itaque nobis soluta reiciendis, ex libero! Quibusdam eligendi
+      ducimus totam, laboriosam similique necessitatibus iure,
+      repellendus officia odio eveniet ratione, labore nihil eius
+      commodi perferendis iste.
+    </p>
+  </div>
+</div>
+<div class="place">
+  <img src="images/Naismith.jpg" alt="James Naismith" />
+  <div class="placeText">
+    <p class="aboutName">James Naismith</p>
+    <p class="aboutInfo">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi
+      eos quis officiis est dicta incidunt aut nulla eaque ullam saepe
+      ipsam quos at laborum excepturi quas repellendus ad, placeat id
+      nihil quidem molestias ut? Doloribus inventore omnis voluptate
+      officia sequi molestias nemo! Vel natus quasi recusandae fuga
+      veniam alias!
+    </p>
+  </div>
+</div>
+<div class="place">
+  <img src="images/wilt.jpg" alt="Wilt Chamberlain" />
+  <div class="placeText">
+    <p class="aboutName">Wilt Chamberlain</p>
+    <p class="aboutInfo">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+      nemo consequuntur quidem rem animi harum doloribus neque qui
+      commodi! Deserunt corrupti, dignissimos incidunt soluta architecto
+      laboriosam repellendus sint pariatur distinctio impedit illum
+      earum inventore ad vitae velit ipsam ipsa molestiae omnis. Quasi
+      blanditiis sed eum. Ex itaque veritatis nisi molestiae magni
+      adipisci culpa, qui eius.
+    </p>
+  </div>
+</div>
+</div>`
+
+var productsContent = `      <div class="products">
+<h1>PRODUCTS PAGE</h1>
+<div class="project">
+  <h2>Product 1</h2>
+  <div class="project-work">
+    <img src="images/tour-04.jpg" alt="Picture4" />
+    <p class="workText">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+      dolor placeat ea omnis quidem dolorem, officiis nam, ducimus quis
+      eos quasi nihil sunt earum reprehenderit perspiciatis tempore.
+      Voluptatem numquam voluptatum dolorem maiores in error aspernatur
+      ducimus eveniet quo, minima et iusto? Recusandae dicta fugiat
+      animi voluptates. Molestias neque deleniti, consequuntur minima
+      optio blanditiis. Necessitatibus veniam reiciendis repudiandae
+      quibusdam iste, quae voluptatem rerum illo eius, in culpa autem
+      doloribus ratione animi magnam laborum ipsum ea fugit. Labore
+      incidunt qui unde architecto omnis ducimus aut laborum a adipisci
+      recusandae deleniti provident aspernatur, molestias itaque ad,
+      facilis debitis ex placeat officiis quod iste repellat. Dolor
+      earum magni, amet maiores dolores quis illo repudiandae rem, iure
+      nobis deleniti a ea quas labore at, odit sint nostrum recusandae
+      veritatis reiciendis ipsum! Quae maiores voluptas nemo!
+    </p>
+  </div>
+</div>
+<div class="project">
+  <h2>Product 2</h2>
+  <div class="project-work">
+    <img src="images/tour-02.jpg" alt="Picture4" />
+    <p class="workText">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore
+      rerum necessitatibus illum cum deleniti explicabo veritatis libero
+      obcaecati ea minima numquam, officia hic voluptas perspiciatis
+      iusto placeat minus magni corrupti sunt ullam nesciunt mollitia
+      magnam quod. Velit alias repellendus modi debitis tenetur adipisci
+      facilis. Magni, laudantium harum. Asperiores, eum optio ea,
+      molestiae autem delectus placeat, ipsam illum sapiente omnis dolor
+      consectetur. Vitae facilis, amet eligendi nemo quae officia, quo
+      praesentium voluptates, sint repellat dolores? Aperiam qui quas
+      quam recusandae quasi, a aliquid quidem sequi eos quos assumenda
+      dignissimos similique quaerat doloribus voluptatibus veniam culpa
+      vel beatae ut nobis? Quos ut tempore impedit fugiat facilis iusto
+      repudiandae aperiam nisi cupiditate blanditiis, expedita odit
+      culpa ipsam reprehenderit aliquam, quod itaque asperiores labore
+      perspiciatis neque tempora dolor. Quasi aspernatur alias a
+      laboriosam eligendi nostrum facilis reiciendis earum reprehenderit
+      temporibus nesciunt, excepturi, ipsam aperiam esse qui sed
+      quaerat, repellendus expedita. Doloribus asperiores, molestias
+      amet nesciunt iure corporis mollitia praesentium sint voluptates,
+      unde debitis dignissimos.
+    </p>
+  </div>
+</div>
+<div class="project">
+  <h2>Product 3</h2>
+  <div class="project-work">
+    <img src="images/tour-08.jpg" alt="Picture4" />
+    <p class="workText">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+      dolor placeat ea omnis quidem dolorem, officiis nam, ducimus quis
+      eos quasi nihil sunt earum reprehenderit perspiciatis tempore.
+      Voluptatem numquam voluptatum dolorem maiores in error aspernatur
+      ducimus eveniet quo, minima et iusto? Recusandae dicta fugiat
+      animi voluptates. Molestias neque deleniti, consequuntur minima
+      optio blanditiis. Necessitatibus veniam reiciendis repudiandae
+      quibusdam iste, quae voluptatem rerum illo eius, in culpa autem
+      doloribus ratione animi magnam laborum ipsum ea fugit. Labore
+      incidunt qui unde architecto omnis ducimus aut laborum a adipisci
+      recusandae deleniti provident aspernatur, molestias itaque ad,
+      facilis debitis ex placeat officiis quod iste repellat.
+    </p>
+  </div>
+</div>
+<div class="project">
+  <h2>Product 4</h2>
+  <div class="project-work">
+    <img src="images/tour-07.jpg" alt="Picture4" />
+    <p class="workText">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+      dolor placeat ea omnis quidem dolorem, officiis nam, ducimus quis
+      eos quasi nihil sunt earum reprehenderit perspiciatis tempore.
+      Voluptatem numquam voluptatum dolorem maiores in error aspernatur
+      ducimus eveniet quo, minima et iusto? Recusandae dicta fugiat
+      animi voluptates. Labore incidunt qui unde architecto omnis
+      ducimus aut laborum a adipisci recusandae deleniti provident
+      aspernatur, molestias itaque ad, facilis debitis ex placeat
+      officiis quod iste repellat. Dolor earum magni, amet maiores
+      dolores quis illo repudiandae rem, iure nobis deleniti a ea quas
+      labore at, odit sint nostrum recusandae veritatis reiciendis
+      ipsum! Quae maiores voluptas nemo!
+    </p>
+  </div>
+</div>
+<div class="project">
+  <h2>Product 5</h2>
+  <div class="project-work">
+    <img src="images/tour-03.jpg" alt="Picture4" />
+    <p class="workText">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+      dolor placeat ea omnis quidem dolorem, officiis nam, ducimus quis
+      eos quasi nihil sunt earum reprehenderit perspiciatis tempore.
+      Voluptatem numquam voluptatum dolorem maiores in error aspernatur
+      ducimus eveniet quo, minima et iusto? Recusandae dicta fugiat
+      animi voluptates. Molestias neque deleniti, consequuntur minima
+      optio blanditiis. Necessitatibus veniam reiciendis repudiandae
+      quibusdam iste, quae voluptatem rerum illo eius, in culpa autem
+      doloribus ratione animi magnam laborum ipsum ea fugit. Labore
+      incidunt qui unde architecto omnis ducimus aut laborum a adipisci
+      recusandae deleniti provident aspernatur, molestias itaque ad,
+      facilis debitis ex placeat officiis quod iste repellat.
+    </p>
+  </div>
+</div>
+</div>`
+
+var contactContent = `      <div class="contact">
+<h1>CONTACT PAGE</h1>
+<div class="contactform">
+  <div class="formInfoMap">
+    <div class="formInfo">
+      <input type="text" placeholder="First Name" class="formInput" />
+      <input type="text" placeholder="Last Name" class="formInput" />
+      <input type="text" placeholder="Email" class="formInput" />
+      <input type="text" placeholder="Phone Number" class="formInput" />
+    </div>
+    <img src="images/travel-fly-map.jpg" alt="Map" />
+  </div>
+  <input type="text" placeholder="Message" class="formMes" />
+  <div class="submit">SUBMIT</div>
+</div>
+</div>`
+
+function initListeners() {
+    $("nav a").on("click", (e) => {
+        let btnID = e.currentTarget.id;
+        let pageContent = btnID + "Content";
+        console.log(eval(pageContent));
+
+        $("#app").html(eval(pageContent));
+    })
+}
+
+$(document).ready(function () {
+    $("#app").html(homeContent);
+    initListeners();
+})
